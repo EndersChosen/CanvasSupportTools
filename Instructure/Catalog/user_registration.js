885967b28ffa7c8a2c9a482c59882dc4af945f31
Caleb Kruger <enderschosen@gmail.com>
2023-03-16 00:27:32 -0600

    created 'registerUser()'

--- Instructure/Catalog/user_registration.js
+++ Instructure/Catalog/user_registration.js
@@ -1,8 +1,35 @@
 // user_registration.js
 const { instance } = require('./config');
+//const { createRequester } = require('../utilities');
+const getRandomPerson = require('../random_user');
 
 const axios = instance;
 
+async function registerUser(name, email, catalog_id = null, custom_fields = null) {
+    // name, email, catalog_id,custom_fields
+    console.log('registering user');
+
+    const userData = {
+        name: name,
+        email: email,
+        catalog_id: catalog_id,
+        custom_fields: {}
+    }
+
+    // post to user_registrations
+    const response = await axios({
+        method: 'POST',
+        url: 'user_registrations',
+        data: userData
+    });
+    return response.data;
+}
+
 (async () => {
-    console.log('This is working');
+    let user = await getRandomPerson.getRandomPerson();
+    console.log(await registerUser(user.firstName + " " + user.lastName, user.email));
 })();
+
+// module.exports = {
+//     registerUser
+// }

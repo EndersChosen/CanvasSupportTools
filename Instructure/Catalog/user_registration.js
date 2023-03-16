4b6c01c30c918bb8a14bd5be3f157d2400ceb105
Caleb Kruger <enderschosen@gmail.com>
2023-03-16 01:02:17 -0600

    Added getUserRegistrations()

--- Instructure/Catalog/user_registration.js
+++ Instructure/Catalog/user_registration.js
@@ -2,8 +2,32 @@
 const { instance } = require('./config');
 //const { createRequester } = require('../utilities');
 const getRandomPerson = require('../random_user');
+const csvExporter = require('../csvExporter');
+const pagination = require('../pagination');
 
 const axios = instance;
+
+async function getUserRegistrations(url = null, users = []) {
+    console.log('getting user registrations');
+
+    let userList = users;
+    let myURL = 'user_registrations?per_page=100';
+    if (url !== null)
+        myURL = url;
+
+
+    const response = await axios({
+        method: 'GET',
+        url: myURL
+    });
+    //console.log(response.data);
+    userList.push(...response.data.user_registrations);
+    let nextPage = await pagination.getNextPage(response.headers.get('link'));
+    if (nextPage !== false) {
+        userList = await getUserRegistrations(nextPage, userList);
+    }
+    return userList;
+}
 
 async function registerUser(name, email, catalog_id = null, custom_fields = null) {
     // name, email, catalog_id,custom_fields
@@ -25,9 +49,24 @@ async function registerUser(name, email, catalog_id = null, custom_fields = null
     return response.data;
 }
 
+async function multiRegister(num) {
+    console.log('registering multiple users');
+
+    for (let i = 0; i < num; i++) {
+        let user = await getRandomPerson.getRandomPerson();
+        await registerUser(user.firstName + " " + user.lastName, user.email)
+    }
+}
+
 (async () => {
-    let user = await getRandomPerson.getRandomPerson();
-    console.log(await registerUser(user.firstName + " " + user.lastName, user.email));
+    csvExporter.exportToCSV(await getUserRegistrations(), 'catalog_registrations');
+    console.log('Done getting registrations');
+
+    // let user = await getRandomPerson.getRandomPerson();
+    // console.log(await registerUser(user.firstName + " " + user.lastName, user.email));
+
+    // await multiRegister(100);
+    // console.log('Done');
 })();
 
 // module.exports = {

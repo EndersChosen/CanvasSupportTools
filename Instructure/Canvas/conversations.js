36c79ff2c650558f9a4c631a332448503f495e07
Caleb Kruger <enderschosen@gmail.com>
2023-04-02 15:25:29 -0600

    added more console.log() to indicate progress

--- Instructure/Canvas/conversations.js
+++ Instructure/Canvas/conversations.js
@@ -19,18 +19,21 @@ async function getConversations(user, url = 'conversations', scope = 'inbox', co
         myURL = url;
     }
 
-    const response = await axios.get(myURL);
-    for (let message of response.data) {
-        myConversations.push(message);
-    }
-
-    let nextPage = pagination.getNextPage(response.headers.get('link'));
-    if (nextPage !== false) {
-        console.log('Page: ', pageCount);
-        pageCount++;
-        myConversations = await getConversations(user, nextPage, null, myConversations, pageCount);
-    } else {
-        console.log('Last page');
+    try {
+        const response = await axios.get(myURL);
+        for (let message of response.data) {
+            myConversations.push(message);
+        }
+        let nextPage = pagination.getNextPage(response.headers.get('link'));
+        if (nextPage !== false) {
+            console.log('Page: ', pageCount);
+            pageCount++;
+            myConversations = await getConversations(user, nextPage, null, myConversations, pageCount);
+        } else {
+            console.log('Last page');
+        }
+    } catch (error) {
+        console.log('ERROR: ', error);
     }
 
     return myConversations;
@@ -43,7 +46,14 @@ async function deleteForAll(conversationID) {
 }
 
 async function bulkDelete(userID, messageFilter) {
-    let allConversations = await getConversations(userID, 'conversations', 'inbox');
+    let allConversations = [];
+
+    // getting all messages in the inbox and sent
+    allConversations.push(await getConversations(userID, 'conversations', 'inbox'));
+    allConversations.push(await getConversations(userID, 'conversations', 'sent'));
+    allConversations = allConversations.flat();
+
+    console.log('done getting converations. Total: ', allConversations.length);
     let myFilter = messageFilter;
     let filteredConversations = [];
     let more = '';
@@ -51,9 +61,17 @@ async function bulkDelete(userID, messageFilter) {
 
     // continue looping for any exta messages that need to be deleted
     while (true) {
+        console.log('filtering converations by ', myFilter);
+        let counter = 0;
         filteredConversations = allConversations.filter((conversation) => {
-            if (conversation.subject === myFilter)
+            if (counter % 100 === 0)
+                console.log('Done with ', counter);
+            // console.log('looking at: ', conversation.id, conversation.subject);
+            if (conversation.subject === myFilter) {
+                console.log('conversation found', conversation.id)
                 return conversation;
+            }
+            counter++;
         });
         let areYouSure = await questionAsker.questionDetails(`Found ${filteredConversations.length} are you sure you want to delete them?(y/n) `);
         if (areYouSure === 'n') {
@@ -139,9 +157,12 @@ async function bulkDelete(userID, messageFilter) {
 
     //await deleteForAll(1466);
 
-    await bulkDelete(26, null)
-    console.log('Finsihed');
+    let user = await questionAsker.questionDetails('What user: ');
+    let filter = await questionAsker.questionDetails('What subject: ');
 
+    await bulkDelete(user, filter)
+    console.log('finished');
+    questionAsker.close();
 })();
 
 // modules.export = {
